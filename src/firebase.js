4cd84be55cb20ccf1de91c7fbbddb5ace3d69d62
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-04-22 17:18:08 +0200

    Cleaning files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD2mithyQ2JrzKUjrk7sYbZibhL3Wdf9vM",
+  authDomain: "clone-58d9b.firebaseapp.com",
+  projectId: "clone-58d9b",
+  storageBucket: "clone-58d9b.appspot.com",
+  messagingSenderId: "76223768242",
+  appId: "1:76223768242:web:370a56214a59f7ca724bab",
+  measurementId: "G-NLD2V25LF2",
+};
